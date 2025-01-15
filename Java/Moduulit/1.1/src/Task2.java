c3ba562fcf293266eb7f0b915b6c9c023eca648d
TheZink <ilkka.sinkonen@gmail.com>
2025-01-15 13:49:57 +0200

    Viikkotehtävä 1.1

--- Java/Moduulit/1.1/src/Task2.java
+++ Java/Moduulit/1.1/src/Task2.java
@@ -1,8 +1,24 @@
 public class Task2 {
     public static void main(String[] args) {
-        for (int i = 1; i < 6; i++) {
-            char star = '*';
-            System.out.println(star*i);
+
+        // Määritetään puun korkeus sekä merkkijonon aloitusmäärä
+        int height = 4;
+        int star = 1;
+
+        for (int i = 1; i <= height; i++) {
+
+            // Lisätään välilyönnit riville
+            for (int k = 1; k <= height - i; k++) {
+                System.out.print(" ");
+            }
+            // Lisätään merkkijono(t) riville
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+
+            }
+            // Tehdään rivinvaihto sekä lisätään merkkijonon määrä
+            star = star + 2;
+            System.out.println();
         }
     }
 }
--- Java/Moduulit/1.1/src/Task3.java
+++ Java/Moduulit/1.1/src/Task3.java
@@ -14,6 +14,8 @@ public class Task3 {
         System.out.println("Give the third number:");
         int third = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The sum of the numbers is: " + (first + second + third)); // Lasketaan lukujen summa
+        System.out.println("Product is: " + (first * second * third)); // Lasketaan lukujen tulo
+        System.out.println("And average is: " + ((first + second + third) / 3.0)); // Lasketaan lukujen keskiarvo
     }
 }
