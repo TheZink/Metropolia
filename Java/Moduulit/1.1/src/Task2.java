ecdc5034d3a7d25a18f412aa725963f44cc40b5e
TheZink <ilkka.sinkonen@gmail.com>
2025-01-15 11:25:41 +0200

    Tehtävät

--- Java/Moduulit/1.1/src/Task2.java
+++ Java/Moduulit/1.1/src/Task2.java
@@ -1,2 +1,8 @@
 public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            char star = '*';
+            System.out.println(star*i);
+        }
+    }
 }
