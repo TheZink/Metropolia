bb4308fea7888063cfa253a4e4d07c69740e44f2
TheZink <ilkka.sinkonen@gmail.com>
2025-01-20 10:47:53 +0200

    Viikkotehtävä 1.1

--- Java/Moduulit/1.1/src/SumOfThreeNumbers.java
+++ Java/Moduulit/1.1/src/SumOfThreeNumbers.java
@@ -19,5 +19,7 @@ public class SumOfThreeNumbers {
         System.out.println("The sum of the numbers is: " + (first + second + third)); // Lasketaan lukujen summa
         System.out.println("Product is: " + (first * second * third)); // Lasketaan lukujen tulo
         System.out.println("And average is: " + ((first + second + third) / 3.0)); // Lasketaan lukujen keskiarvo
+
+        scanner.close();
     }
 }
