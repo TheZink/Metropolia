494de6be7b0475bd17989c806ebf6c73b0b0adef
TheZink <ilkka.sinkonen@gmail.com>
2025-03-04 19:00:54 +0200

    Viikkotehtävä 7.1

--- Java/Moduulit/7.1/task/sql_query.sql
+++ Java/Moduulit/7.1/task/sql_query.sql
@@ -2,6 +2,6 @@ SELECT * FROM Currency;
 SELECT name, value FROM Currency where name = 'EUR';
 SELECT name, value FROM Currency where value > 1.0;
 
-SELECT name, value FROM Currency where max(SELECT MAX(value) from Currency);
+SELECT name, value FROM Currency WHERE value = (SELECT MAX(value) from Currency);
 
 SELECT COUNT(*) as count from Currency;
