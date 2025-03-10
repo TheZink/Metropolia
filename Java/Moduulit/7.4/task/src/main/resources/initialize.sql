abc5963a41fcafcb0d3244970842296794da0279
TheZink <ilkka.sinkonen@gmail.com>
2025-03-10 13:45:41 +0200

    Muutin SQL scriptin

--- Java/Moduulit/7.4/task/src/main/resources/initialize.sql
+++ Java/Moduulit/7.4/task/src/main/resources/initialize.sql
@@ -1,13 +1,31 @@
-USE converter;
+DROP DATABASE IF EXISTS CONVERTER;
+CREATE DATABASE CONVERTER;
 
-DROP TABLE IF EXISTS currency;
+DROP USER IF EXISTS 'appuser'@'localhost';
+CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT ON CONVERTER.* TO 'appuser'@'localhost';
 
-CREATE TABLE IF NOT EXISTS currency (
+
+USE CONVERTER;
+
+DROP TABLE IF EXISTS Currency;
+
+CREATE TABLE IF NOT EXISTS Currency (
     id INT PRIMARY KEY AUTO_INCREMENT,
     code VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     rates DECIMAL(10,4) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS transaction(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	transactionDate VARCHAR(255),
+	FromCurrency VARCHAR(255),
+	ToCurrency VARCHAR(255),
+	FromAmount DECIMAL(10,4),
+	totalAmount DECIMAL(10,4)
+);
+	
 
 INSERT INTO Currency (code, name ,rates) VALUES ('USD', 'US Dollar', 1.0);
 INSERT INTO Currency (code, name ,rates) VALUES ('EUR', 'Euro', 0.9);
@@ -19,3 +37,9 @@ INSERT INTO Currency (code, name ,rates) VALUES ('INR', 'Indian Rupee', 70.0);
 INSERT INTO Currency (code, name ,rates) VALUES ('KRW', 'South Korean Won', 1200.0);
 INSERT INTO Currency (code, name ,rates) VALUES ('GBP', 'British Pound', 0.8);
 INSERT INTO Currency (code, name ,rates) VALUES ('JPY', 'Japanese Yen', 110.0);
+
+
+
+
+
+
