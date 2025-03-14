e977ecb9d85b86defdace70852f02dccdc1ae8aa
TheZink <ilkka.sinkonen@gmail.com>
2025-03-14 11:26:17 +0200

    Viikkotehtävä 8.3

--- Java/Moduulit/8.3/Collection.java
+++ Java/Moduulit/8.3/Collection.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
 public class Collection {
-    public Collection() {
-        List<Integer> numbers = n;
-
+    
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(10, 5, 8, 20, 15, 3, 12));
+        final int[] sum = {0};
+    
+        numbers.removeIf(n -> n % 2 == 0); // To remove a even numbers
+        numbers.replaceAll(n -> n * 2); // double a value of odd numbers
+        numbers.forEach(n -> sum[0] += n); // calculate all numbers in the list
+        
+        System.out.println(sum[0]);
     }
-
-
 }
